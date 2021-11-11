a807ca18fda66840b7afab37b45128cdeb1b45be
NaikyDing <74ding@gmail.com>
2021-11-11 17:51:13 +0800

    feat(home): navbar

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,17 +1,25 @@
 <script setup>
-import { onMounted, reactive } from 'vue'
+import { onMounted, reactive, ref } from 'vue'
 import { getAllTourismAPI, getTourismAPI } from '../request/api'
-import axios from 'axios'
-import { getHeaders } from '../request/headers'
-
 
 const state = reactive({
   data: []
 })
 
+const webSiteName = ref('TAIWAN TRAVEL')
+
+const navs = reactive([
+  '旅遊情報',
+  '景點查詢',
+  '美食推薦',
+  '旅宿資訊',
+  '節慶活動'
+])
+
 onMounted(async() => {
   const res = await getAllTourismAPI({})
-  state.data = res.data
+  await getTourismAPI({})
+  // state.data = res.data
 })
 
 // console.log(tourism)
@@ -19,8 +27,14 @@ onMounted(async() => {
 
 
 <template>
-  <h1>HOME</h1>
-  <p>{{ state.data }}</p>
+  <nav class="bg-red-50">
+    <ul class="flex justify-center items-center h-20 px-36">
+      <li class="text-white text-2xl flex-grow">{{ webSiteName }}</li>
+      <li class="px-6 text-white" v-for="nav in navs" :key="nav">
+        <a href="#">{{ nav }}</a>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
