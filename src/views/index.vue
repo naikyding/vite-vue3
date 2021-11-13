c2f9c2fc764833522dc6d917e5862272eca21a72
naiky <74ding@gmail.com>
2021-11-13 17:13:46 +0800

    feat: mobile nav

--- src/views/index.vue
+++ src/views/index.vue
@@ -19,6 +19,9 @@ const state = reactive({
     '旅宿資訊',
     '節慶活動'
   ],
+  mobile: {
+    navStatus: false 
+  },
   cities
 })
 
@@ -35,14 +38,29 @@ onMounted(async() => {
 
 <template>
   <div class="min-h-screen bg-center bg-cover flex items-center justify-center flex-col" :style="state.website.cover">
+    <!-- mobile nav -->
+    <div v-show="state.mobile.navStatus" class="bg-white absolute z-10 top-0 left-0 p-4 w-full">
+      <div @click="state.mobile.navStatus = false" class="close-btn">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </div>
+      <ul class="px-16">
+        <li class="block text-center text-primary3 cursor-pointer hover:text-white py-4 hover:bg-primary rounded-lg my-4" v-for="nav in state.navs" :key="nav">
+          <a class=" text-shadow-lg tracking-wider" href="#">{{ nav }}</a>
+        </li>
+      </ul>
+    </div>
 
     <!-- web nav -->
     <nav class="absolute top-0 inset-x-0 flex justify-between items-center px-4 pt-4 sm:pt-0 sm:px-8 xl:px-36">
-      <button class="text-white sm:hidden">
+      <!-- mobile menu btn -->
+      <button @click="state.mobile.navStatus = true" class="text-white sm:hidden">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" fill="red" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
         </svg>
       </button>
+
       <!-- website logo -->
       <a href="/" class="text-white text-2xl">{{ state.website.name }}</a>
 
