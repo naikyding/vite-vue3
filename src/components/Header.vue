bb626aa7fdd2d425fc2cf9c86544b7a52dea6514
naiky <74ding@gmail.com>
2021-11-14 15:05:01 +0800

    feat: header組件抽離

---
+++ src/components/Header.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { reactive } from "vue";
+
+const state = reactive({
+  data: [],
+  website: {
+    name: 'TAIWAN TRAVEL',
+  },
+  navs: [
+    '旅遊情報',
+    '景點查詢',
+    '美食推薦',
+    '旅宿資訊',
+    '節慶活動'
+  ],
+  mobile: {
+    navStatus: false 
+  },
+})
+
+</script>
+
+<template>
+	<!-- mobile nav -->
+	<div v-show="state.mobile.navStatus" @click="state.mobile.navStatus = false" class="absolute z-10 bg-black opacity-50 inset-0"></div>
+	<div v-show="state.mobile.navStatus" class="bg-white absolute z-20 top-0 left-0 p-4 w-full">
+		<div @click="state.mobile.navStatus = false" class="close-btn">
+			<svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+			</svg>
+		</div>
+		<ul class="px-16">
+			<li class="block text-center text-primary3 cursor-pointer hover:text-white py-4 hover:bg-primary rounded-lg my-4" v-for="nav in state.navs" :key="nav">
+				<a class=" text-shadow-lg tracking-wider" href="#">{{ nav }}</a>
+			</li>
+		</ul>
+	</div>
+
+	<!-- web nav -->
+	<nav class="absolute top-0 inset-x-0 flex justify-between items-center px-4 pt-4 sm:pt-0 sm:px-8 xl:px-36">
+		<!-- mobile menu btn -->
+		<button @click="state.mobile.navStatus = true" class="text-white sm:hidden">
+			<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+				<path stroke-linecap="round" stroke-linejoin="round" fill="red" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+			</svg>
+		</button>
+
+		<!-- website logo -->
+		<a href="/" class="text-white text-2xl">{{ state.website.name }}</a>
+
+		<!-- nav items -->
+		<ul class="items-center h-20 hidden sm:flex">
+			<li class="sm:px-1 lg:px-6" v-for="nav in state.navs" :key="nav">
+				<a class="text-white text-shadow-lg tracking-wider" href="#">{{ nav }}</a>
+			</li>
+		</ul>
+	</nav>
+</template>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, reactive, ref } from 'vue'
 import { getAllTourismAPI, getTourismAPI } from '../request/api'
+import Header from '../components/Header.vue'
 
 import cover1 from '../assets/images/cover1.jpeg'
 import taiwanLogo from '../assets/images/taiwan_logo_white.svg'
@@ -9,18 +10,7 @@ import cities from '../utils/cityData'
 const state = reactive({
   data: [],
   website: {
-    name: 'TAIWAN TRAVEL',
     cover: { backgroundImage: `url(${cover1})`},
-  },
-  navs: [
-    '旅遊情報',
-    '景點查詢',
-    '美食推薦',
-    '旅宿資訊',
-    '節慶活動'
-  ],
-  mobile: {
-    navStatus: false 
   },
   cities
 })
@@ -29,49 +19,15 @@ const state = reactive({
 onMounted(async() => {
   const res = await getAllTourismAPI({})
   await getTourismAPI({})
-  // state.data = res.data
 })
 
-// console.log(tourism)
 </script>
 
 
 <template>
   <div class="min-h-screen bg-center bg-cover flex items-center justify-center flex-col relative" :style="state.website.cover">
-    <!-- mobile nav -->
-    <div v-show="state.mobile.navStatus" @click="state.mobile.navStatus = false" class="absolute z-10 bg-black opacity-50 inset-0"></div>
-    <div v-show="state.mobile.navStatus" class="bg-white absolute z-20 top-0 left-0 p-4 w-full">
-      <div @click="state.mobile.navStatus = false" class="close-btn">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-        </svg>
-      </div>
-      <ul class="px-16">
-        <li class="block text-center text-primary3 cursor-pointer hover:text-white py-4 hover:bg-primary rounded-lg my-4" v-for="nav in state.navs" :key="nav">
-          <a class=" text-shadow-lg tracking-wider" href="#">{{ nav }}</a>
-        </li>
-      </ul>
-    </div>
-
-    <!-- web nav -->
-    <nav class="absolute top-0 inset-x-0 flex justify-between items-center px-4 pt-4 sm:pt-0 sm:px-8 xl:px-36">
-      <!-- mobile menu btn -->
-      <button @click="state.mobile.navStatus = true" class="text-white sm:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" fill="red" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-        </svg>
-      </button>
-
-      <!-- website logo -->
-      <a href="/" class="text-white text-2xl">{{ state.website.name }}</a>
-
-      <!-- nav items -->
-      <ul class="items-center h-20 hidden sm:flex">
-        <li class="sm:px-1 lg:px-6" v-for="nav in state.navs" :key="nav">
-          <a class="text-white text-shadow-lg tracking-wider" href="#">{{ nav }}</a>
-        </li>
-      </ul>
-    </nav>
+    <!-- header -->
+    <Header />
 
     <section class="text-center w-full">
       <!-- taiwan logo -->
