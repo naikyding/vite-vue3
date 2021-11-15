1064b0e37bb6690287c69dd30ac3c595b54331ec
NaikyDing <74ding@gmail.com>
2021-11-15 17:19:47 +0800

    feat(store): 新增需得指定城市資料 api && 功能

--- src/store/index.js
+++ src/store/index.js
@@ -18,6 +18,11 @@ const actions = {
 	async get_all_tourism({commit}, filterData) {
 		const res = await getAllTourismAPI(filterData)
 		if(res.status === 200) commit('SET_SEARCH_TOURISM', res.data)
+	},
+
+	async get_tourism({ commit }, filterData) {
+		const res = await getTourismAPI(filterData)
+		if(res.status === 200) commit('SET_SEARCH_TOURISM', res.data)
 	}
 }
 
