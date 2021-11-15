047ee009d85493c28d6959b2675d5119736d351b
NaikyDing <74ding@gmail.com>
2021-11-15 17:20:35 +0800

    feat (attractions): 景點查詢
    - 改由指定城市取得資料
    - 串接 api 資料
    - layout 調整

--- src/views/Attractions/index.vue
+++ src/views/Attractions/index.vue
@@ -12,23 +12,30 @@ import noImage from '../../assets/icon/no-image.svg'
 const store = useStore()
 
 // 取得所有旅遊景點
-const getAllTourism = () => store.dispatch('get_all_tourism', {$top: 8})
-getAllTourism()
+// const getAllTourism = () => store.dispatch('get_all_tourism', {$top: 8})
+const getOneCityTourism = (filterData) => store.dispatch('get_tourism', filterData)
+// getAllTourism()
 
 const state = reactive({
   cover: {backgroundImage: `url(${coverImg})`},
   title: ['熱門景點', '快速搜尋', '搜尋結果'],
   cities,
   class: [
-    {'全部': 'all'},
-    {'全部': 'all'},
-    {'全部': 'all'},
-  ]
+    {'option1': '1'},
+    {'option2': '2'},
+    {'option3': '3'},
+  ],
+
+  form: {
+    city: 'Taipei',
+    class: 'all'
+  }
 })
 
 const stateTourismData = computed(() => store.state.attractions)
 
 onMounted(() => {
+  getOneCityTourism({city: state.form.city, $top: 8})
 })
 </script>
 
@@ -107,7 +114,7 @@ onMounted(() => {
           <h2 class="text-xl text-secondary mb-2">縣市</h2>
           <!-- select -->
           <div class="select-area w-full relative">
-            <select name="" class="w-full appearance-none bg-input-bg border text-gray-700 py-3 px-4 pr-8 rounded-xl leading-tight focus:outline-none focus:border-primary">
+            <select v-model="state.form.city" class="w-full appearance-none bg-input-bg border text-gray-700 py-3 px-4 pr-8 rounded-xl leading-tight focus:outline-none focus:border-primary">
               <option v-for="(city, index) in cities" :key="city" :value="city">{{ index }}</option>
             </select>
             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-black">
@@ -117,9 +124,17 @@ onMounted(() => {
           
           <hr class="my-4 border-1 bg-gray-200">
 
+          <!-- 類別 -->
           <h2 class="text-xl text-secondary mb-2">類別</h2>
+
+          <!-- 全部 類別 -->
+          <label class="block">
+            <input v-model="state.form.class" value="all" class="mr-2" name="class" type="radio">
+            <span class="text-primary3">全部</span>
+          </label>
+
           <label class="block" v-for="item in state.class" :key="item">
-            <input class="mr-2" name="class" type="radio" >
+            <input v-model="state.form.class" :value="Object.values(item)[0]" class="mr-2" name="class" type="radio" >
             <span class="text-primary3">{{ Object.keys(item)[0]}}</span>
           </label>
 
@@ -156,26 +171,50 @@ onMounted(() => {
 
         <!-- result items -->
         <div class="result-items col-span-3 mt-8">
-          <figure class="bg-white border shadow-lg rounded-2xl flex overflow-hidden mb-10 h-60" v-for="item in stateTourismData" :key="item.ID">
-            <img class="h-auto w-80" :src="item.Picture.PictureUrl1 || noImage" :alt="item.Name">
-            <figcaption class="px-4 py-5 flex-1">
-              <div class="figcaption-header flex justify-between mb-2">
-                <h2 class="block text-xl text-secondary">
+          <figure class="bg-white border shadow-lg rounded-2xl flex overflow-hidden mb-10 h-32 md:h-60" v-for="item in stateTourismData" :key="item.ID">
+            <img class="h-auto object-cover object-center w-40 lg:w-80" :src="item.Picture.PictureUrl1 || noImage" :alt="item.Name">
+            <figcaption class="px-2 md:px-4 py-3 md:py-5 flex-1 flex flex-col">
+              <!-- TITLE && TAGS -->
+              <div class="flex-grow md:flex-grow-0 figcaption-header flex flex-col justify-between mb-2">
+                <h2 class="block text-base md:text-xl text-secondary">
                   {{ item.Name }}
                 </h2>
                 <!-- TAG (CLASS) -->
-                <div v-if="item.Class1" class="tag bg-primary text-white px-3 flex items-center rounded-full">
-                  {{ item.Class1}}
+                <div class="tag-area hidden md:flex">
+                  <span v-if="item.Class1" class="text-center text-xs tag bg-primary text-white px-3 py-1 md:flex items-center rounded-full">
+                    {{ item.Class1}}
+                  </span>
+                  <span v-if="item.Class2" class="text-center text-xs mx-2 tag bg-primary text-white px-3 py-1 md:flex items-center rounded-full">
+                    {{ item.Class2}}
+                  </span>
+                  <span v-if="item.Class3" class="text-center text-xs tag bg-primary text-white px-3 py-1 md:flex items-center rounded-full">
+                    {{ item.Class3}}
+                  </span>
                 </div>
-                <div v-if="item.Class2" class="tag bg-primary text-white px-3 flex items-center rounded-full">
-                  {{ item.Class2}}
-                </div>
-                <div v-if="item.Class3" class="tag bg-primary text-white px-3 flex items-center rounded-full">
-                  {{ item.Class3}}
-                </div>
+                
               </div>
-              <div class="description text-gray-500">
-                {{ item.Description.length > 70 ? item.Description.slice(0, 70) + '...' : item.Description }}
+              
+              <!-- 說明 -->
+              <div class="hidden md:block description text-gray-500 flex-grow">
+                {{ item.DescriptionDetail.length > 70 ? item.DescriptionDetail.slice(0, 70) + '...' : item.DescriptionDetail }}
+              </div>
+
+              <!-- 標記 -->
+              <div class="flex">
+                <!-- 城市 -->
+                <div class="location flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-priamry2 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+                  </svg>
+                  <span class="text-gray-500">{{ item.City }}</span>
+                </div>
+                <!-- 開放時間 -->
+                <div class="hidden md:flex open-time ml-4 items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-priamry2 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clip-rule="evenodd" />
+                  </svg>
+                  <span class="text-gray-500">{{ item.OpenTime?.length > 10 ? item.OpenTime.slice(0, 10) + '...' : item.OpenTime }}</span>
+                </div>
               </div>
             </figcaption>
           </figure>
