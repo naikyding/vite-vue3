55168d9ed3b898ab6274f7056c7f03b4a4928f0d
NaikyDing <74ding@gmail.com>
2021-11-16 17:48:13 +0800

    feat: 分頁

---
+++ src/components/PagePagination.vue
@@ -0,0 +1,29 @@
+<script setup>
+// eslint-disable-next-line no-undef
+defineProps({
+  allPage: {
+    required: true,
+    type: Number,
+  },
+})
+</script>
+
+<template>
+  <ul class="flex">
+    <li class="border rounded-lg h-10 w-10 flex justify-center items-center">
+      ←
+    </li>
+    <li
+      v-for="(item, index) in parseInt(allPage)"
+      :key="index"
+      class="border rounded-lg h-10 w-10 flex justify-center items-center"
+    >
+      {{ index + 1 }}
+    </li>
+    <li class="border rounded-lg h-10 w-10 flex justify-center items-center">
+      →
+    </li>
+  </ul>
+</template>
+
+<style lang="scss" scoped></style>
