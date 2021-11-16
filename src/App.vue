45607980b9c40e811183906c9ca8ccf33618e328
NaikyDing <74ding@gmail.com>
2021-11-16 15:47:45 +0800

    fix: nav

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import Header from '@/components/Header.vue'
+import LayoutHeader from '@/components/LayoutHeader.vue'
+import LayoutFooter from '@/components/LayoutFooter.vue'
 </script>
 
 <template>
-  <Header />
-  <router-view />
-  <Footer />
+  <div class="font-sans">
+    <LayoutHeader />
+    <router-view />
+    <LayoutFooter />
+  </div>
 </template>
 
 <style></style>
---
+++ src/components/LayoutFooter.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div
+    class="
+      bg-priamry2
+      h-20
+      flex
+      justify-center
+      items-center
+      text-white
+      tracking-widest
+    "
+  >
+    TaiwanTravel © 2021 Designer Vum. Engineer Naiky. All rights reserved.
+  </div>
+</template>
