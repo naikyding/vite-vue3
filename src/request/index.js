d4a6df19c8e25a7f447f54b1614622d57bdbee7d
NaikyDing <74ding@gmail.com>
2021-11-17 16:38:43 +0800

    feat: 請求前更新 headers

--- src/request/index.js
+++ src/request/index.js
@@ -1,16 +1,14 @@
 import axios from 'axios'
 import { getHeaders } from './headers'
 
-
 // 取得所有[ ]觀光景點資料
 export const request = axios.create({
   baseURL: 'https://ptx.transportdata.tw/MOTC/v2/Tourism/',
   timeout: 50000, // request timeout
-  headers: getHeaders()
 })
 
 request.interceptors.request.use((config) => {
-  // console.log('request')
+  config.headers = getHeaders()
   return config
 })
 
