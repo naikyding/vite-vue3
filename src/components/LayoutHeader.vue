ee7b6675d2d7753992327e56460cface03af95cb
naiky <74ding@gmail.com>
2021-11-20 11:24:56 +0800

    feat: nav website logo

--- src/components/LayoutHeader.vue
+++ src/components/LayoutHeader.vue
@@ -147,7 +147,21 @@ onMounted(() => {
       </button>
 
       <!-- website logo -->
-      <router-link to="/" class="text-2xl" :class="navDarkText()">
+      <router-link
+        to="/"
+        class="text-2xl flex items-center font-bold font-momo"
+        :class="navDarkText()"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-8 w-8"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"
+          />
+        </svg>
         {{ state.website.name }}
       </router-link>
 
