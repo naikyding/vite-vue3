dc020fa2a359be3d9978869a373698bcbfc7bd8d
naiky <74ding@gmail.com>
2021-11-20 22:29:47 +0800

    feat: 導引地圖 && 撥打電話

--- src/views/Restaurant/index.vue
+++ src/views/Restaurant/index.vue
@@ -48,6 +48,13 @@ function goSectionArea() {
   gsap.to(window, { duration: 0.6, ease: 'power1', scrollTo: target })
 }
 
+function goGoogleMap(name, city) {
+  window.open(
+    `https://www.google.com/maps/search/?api=1&query=${name + city}`,
+    'blacnk'
+  )
+}
+
 onMounted(() => {
   store.dispatch('get_city_restaurant', state.form)
 })
@@ -176,7 +183,10 @@ onMounted(() => {
           <div class="flex h-full flex-col justify-between">
             <div class="btn px-7 pt-7">
               <!-- MAP -->
-              <button class="border rounded-full p-1 mr-3">
+              <button
+                class="border rounded-full p-1 mr-3"
+                @click="goGoogleMap(item.Name, item.City)"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   class="h-5 w-5"
@@ -193,16 +203,18 @@ onMounted(() => {
 
               <!-- PHONE -->
               <button class="border rounded-full p-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  class="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                >
-                  <path
-                    d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"
-                  />
-                </svg>
+                <a :href="`tel:${item.Phone}`">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="h-5 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"
+                    />
+                  </svg>
+                </a>
               </button>
             </div>
             <div class="text text-center mb-8 px-2">
