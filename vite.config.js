88e252f1ba34cb3972014bf1a05d48fdcab0d957
NaikyDing <74ding@gmail.com>
2021-12-07 13:35:30 +0800

    remove test

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import eslintPlugin from 'vite-plugin-eslint'
+import eslintPlugin from 'vite-plugin-eslint'
 
 const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 資料夾別名
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [
-    vue(),
-    //  eslintPlugin()
-  ],
+
+  // eslint 格式化套件
+  plugins: [vue(), eslintPlugin()],
 })
